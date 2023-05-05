4ecb8ae4ccf15ae94f9bf3bbd38fb09b153accb9
sanket723 <sanketpawar723@gmail.com>
2023-05-05 16:03:17 +0530

    check productionHouse

--- src/main/java/com/driver/services/ProductionHouseService.java
+++ src/main/java/com/driver/services/ProductionHouseService.java
@@ -17,15 +17,15 @@ public class ProductionHouseService {
 
     public Integer addProductionHouseToDb(ProductionHouseEntryDto productionHouseEntryDto){
 
-        ProductionHouse productionHouse = new ProductionHouse();
-        productionHouse.setName(productionHouseEntryDto.getName());
-        productionHouse.setRatings(0);
-        productionHouse.setWebSeriesList(new ArrayList<>());
+        ProductionHouse productionHouse = new ProductionHouse(productionHouseEntryDto.getName());
+       // productionHouse.setName(productionHouseEntryDto.getName());
+        productionHouse.setRatings(0.0);
+       // productionHouse.setWebSeriesList(new ArrayList<>());
 
-        productionHouseRepository.save(productionHouse);
+        ProductionHouse addedProductionHouse = productionHouseRepository.save(productionHouse);
 
 
-        return productionHouse.getId();
+        return addedProductionHouse.getId();
     }
 
 
