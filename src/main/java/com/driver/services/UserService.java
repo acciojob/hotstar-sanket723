2e6d06a79cfc9819bb88d85c8610eabd343fdba5
sanket723 <sanketpawar723@gmail.com>
2023-05-05 15:54:08 +0530

    first try

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -25,7 +25,9 @@ public class UserService {
     public Integer addUser(User user){
 
         //Jut simply add the user to the Db and return the userId returned by the repository
-        return null;
+        User newUser = userRepository.save(user);
+
+        return newUser.getId();
     }
 
     public Integer getAvailableCountOfWebSeriesViewable(Integer userId){
@@ -33,8 +35,43 @@ public class UserService {
         //Return the count of all webSeries that a user can watch based on his ageLimit and subscriptionType
         //Hint: Take out all the Webseries from the WebRepository
 
+        User user = userRepository.findById(userId).get();
 
-        return null;
+        Subscription subscription = user.getSubscription();
+
+        SubscriptionType subscriptionType = subscription.getSubscriptionType();
+
+        List<WebSeries> webSeriesList = webSeriesRepository.findAll();
+
+//        int basicCount=0;
+//        int proCount=0;
+//        int eliteCount=0;
+
+         int count=0;
+
+        if(subscriptionType.toString().equals("BASIC")){
+            for(WebSeries web : webSeriesList){
+                if(web.getSubscriptionType().toString().equals("BASIC"))
+                    count++;
+                   // basicCount++;
+            }
+        }
+        else if (subscriptionType.toString().equals("PRO")) {
+            for(WebSeries web : webSeriesList){
+                if(web.getSubscriptionType().toString().equals("PRO") || web.getSubscriptionType().toString().equals("BASIC") )
+                     count++;
+                    // proCount++;
+            }
+        }
+        else {
+            for(WebSeries web : webSeriesList){
+                count++;
+//                if(web.getSubscriptionType().toString().equals("ELITE"))
+//                    eliteCount++;
+            }
+        }
+
+        return count;
     }
 
 
